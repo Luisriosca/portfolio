5ea8c7101d9892102b5e0e893feee3cffee50bd4
Luisriosca <luis.rios.9815@gmail.com>
2022-07-14 00:58:47 -0500

    Making the chat msg component and implement a simple method to check if a msg its mine or not

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
+Vue.component('msg-text', {
+    props: {
+        text: String,
+        emitter: String
+    }, 
+    template: '<div class="msg col-12" v-bind:class="emitter">{{ text }}</div>',
+    created() {
+        // props are exposed on `this`
+        console.log(this.emitter)
+    }
+});
+
 new Vue({
     el: "#app",
     data: {
-        msg: "Vue js Hello World - Your First Vue.js App"
+        msgList: [{
+                emitter: "user",
+                text: "Hola, estoy buscando un desarrollador. Tengo una increible idea y necesito tu ayuda para materializarla! "
+            },
+            {
+                emitter: "me",
+                text: "Hola qué tal, mi nombre es Luis y soy desarrollador web fullstack ¿Cómo puedo ayudarte? "
+            },
+            {
+                emitter: "me",
+                text: "Me especializo en desarrollo de interfaces web y single page applications, me gusta mucho usar Vue y nodejs para mis proyectos  "
+            },
+        ]
+    },
+    methods: {
+
     }
 });
